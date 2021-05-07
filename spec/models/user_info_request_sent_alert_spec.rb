d220a27bb35c4a0bfbafcf38618bcd516d68a0fb
Graeme Porteous <graeme@rgbp.co.uk>
2021-05-07 06:48:30 +0100

    Add recent scope to user info requests send alert

--- spec/models/user_info_request_sent_alert_spec.rb
+++ spec/models/user_info_request_sent_alert_spec.rb
@@ -15,6 +15,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 RSpec.describe UserInfoRequestSentAlert do
+  describe 'scopes' do
+    let!(:recent) do
+      FactoryBot.create(:user_info_request_sent_alert, created_at: 1.year.ago)
+    end
+
+    let!(:old) do
+      FactoryBot.create(:user_info_request_sent_alert, created_at: 366.days.ago)
+    end
+
+    it 'can scope to recent sent alerts' do
+      expect(described_class.recent).to include(recent)
+      expect(described_class.recent).to_not include(old)
+    end
+  end
+
   it 'should allow an alert type of "survey_1"' do
     alert = UserInfoRequestSentAlert.new(alert_type: 'survey_1')
     expect(alert).to be_valid
